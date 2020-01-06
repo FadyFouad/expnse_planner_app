5129425737d86644fff0b0c9beae7bbb0e4104f1
Fady Fouad <fady.fouad.a@gmail.com>
2020-01-07 00:39:26 +0200

    ListView.builder

--- lib/widgets/tx_list.dart
+++ lib/widgets/tx_list.dart
@@ -8,52 +8,55 @@ import '../transaction.dart';
 ///***************************************************/
 
 class TxList extends StatelessWidget {
-
   final List<Transaction> transactions;
 
   const TxList({Key key, this.transactions}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    var transaction;
     return Container(
-        child: Column(
-      children: transactions.map((transaction) {
-        return Card(
-          margin: EdgeInsets.all(8),
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Column(
-                  children: <Widget>[
-                    Text(
-                      '${transaction.title}',
-                      style: TextStyle(
-                          color: Colors.black, fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      '${DateFormat().add_yMMMd().format(transaction.date)}',
-                      style: TextStyle(color: Colors.grey),
-                    ),
-                  ],
-                ),
-                Container(
-                  child: Text(
-                    ' ${transaction.price} \$',
-                    style: (TextStyle(
-                      fontSize: 16,
-                      color: Colors.purple,
-                    )),
+      height: 350,
+      child: ListView.builder(
+        itemBuilder: (BuildContext context, int index) {
+          return Card(
+            margin: EdgeInsets.all(8),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      Text(
+                        '${transactions[index].title}',
+                        style: TextStyle(
+                            color: Colors.black, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        '${DateFormat().add_yMMMd().format(
+                            transactions[index].date)}',
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                    ],
                   ),
-                  padding: EdgeInsets.all(4),
-                ),
-              ],
+                  Container(
+                    child: Text(
+                      ' ${transactions[index].price} \$',
+                      style: (TextStyle(
+                        fontSize: 16,
+                        color: Colors.purple,
+                      )),
+                    ),
+                    padding: EdgeInsets.all(4),
+                  ),
+                ],
+              ),
             ),
-          ),
-        );
-      }).toList(),
-    ));
+          );
+        },
+        itemCount: transactions.length,
+
+      ),
+    );
   }
 }
